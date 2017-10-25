9fb9515ff5c806de01df73ec5a0674ac294bf105
Greg <myersg86@gmail.com>
2017-10-24 20:35:08 -0700

    update css

--- functions.php
+++ functions.php
@@ -270,8 +270,3 @@ function genesis_sample_footer_creds_filter( $creds ) {
 	return $creds;
 }
 
-function custom_style_sheet() {
-	wp_enqueue_style( 'custom-styling', get_stylesheet_directory_uri() . '/custom.css' );
-	wp_enqueue_style( 'font-styling', 'https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800' );
-}
-add_action('wp_enqueue_scripts', 'custom_style_sheet');
